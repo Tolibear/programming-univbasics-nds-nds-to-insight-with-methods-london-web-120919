ff3ad8661d5e2b032a4978fc7078d21b16a2fa4d
T Benson <t.benson@live.co.uk>
2019-12-02 13:58:22 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -15,9 +15,11 @@ end
 # using director_data as input
 def gross_for_director(director_data)
   director_gross = 0
+  director_index = 0
   movie_index = 0
-  while movie_index < director_data[:movies].count do
-    director_gross += director_data[:movies][movie_index][:worldwide_gross]
+  while movie_index < directors_database[director_index][:movies].count do
+    director_gross += directors_database[director_index][:movies][movie_index][:worldwide_gross]
+    movie_index += 1
   end
   director_gross
 end
